291e7c632c915a139f8b7be145bb49eb367b3274
hun24 <100418580+hun24@users.noreply.github.com>
2022-02-26 15:53:43 +0900

    Update and rename README.md to form.py

---
+++ form.py
@@ -0,0 +1,4 @@
+class UserLoginForm(FlaskForm):
+    username = StringField('사용자이름', validators=[DataRequired(), Length(min=3, max=25)])
+    password = PasswordField('비밀번호', validators=[DataRequired()])
+    
