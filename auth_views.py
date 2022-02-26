13b4e27bf88d531beee65679b161e4f1258aa64b
hun24 <100418580+hun24@users.noreply.github.com>
2022-02-26 15:54:10 +0900

    Create auth_views.py

---
+++ auth_views.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, url_for, render_template, flash, request, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import redirect
+
+from pybo import db
+from pybo.forms import UserCreateForm, UserLoginForm
+
+(... 생략 ...)
+
+@bp.route('/login/', methods=('GET', 'POST'))
+def login():
+    form = UserLoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        error = None
+        user = User.query.filter_by(username=form.username.data).first()
+        if not user:
+            error = "존재하지 않는 사용자입니다."
+        elif not check_password_hash(user.password, form.password.data):
+            error = "비밀번호가 올바르지 않습니다."
+        if error is None:
+            session.clear()
+            session['user_id'] = user.id
+            return redirect(url_for('main.index'))
+        flash(error)
+    return render_template('auth/login.html', form=form)
